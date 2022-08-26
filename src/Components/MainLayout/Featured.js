b9aa94e60c8944de5ae008ba5e0d3ade108019a9
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-08-27 03:38:54 +0800

    changed what we do phrase

--- src/Components/MainLayout/Featured.js
+++ src/Components/MainLayout/Featured.js
@@ -4,7 +4,7 @@ const Featured = () => {
     return (
         <div className="featured" id = "whatWeDo">
             <h1>What We Do</h1>
-            <p className = "font-memphis">Perhaps an example is a thousand times greater than words. </p>
+            <p className = "font-memphis">If a picture is worth a thousand words, then a video is worth a million. </p>
             <p >Check out our featured video!</p>
             <video controls>
                 <source src={featured} type="video/mp4" />
