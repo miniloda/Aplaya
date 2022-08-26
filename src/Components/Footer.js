b4ff83013c5ad1ef201c953f153aded90a69f6e8
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-08-27 06:46:27 +0800

    added git repo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@mui/material/Box';
 import React from 'react';
 import Link from '@mui/material/Link';
+import Git_icon from "./MainLayout/assets/Git_icon.svg"
 const Footer = () => {
     return(
         <>
@@ -9,7 +10,11 @@ const Footer = () => {
         <Grid container spacing={3} sx ={{position: "absolute", bottom: 0}}>
             <Grid item xs={12}>
                 <Box sx = {{display: "flex", alignItems: "center", justifyContent: "center", width: "100%"}}>
-                    <span>Designed and built by <Link href = "https://github.com/miniloda" noopener="true" target="_blank"> miniloda</Link></span>
+                    <span>Designed and built by <Link href = "https://github.com/miniloda" noopener="true" target="_blank"> miniloda</Link>
+                    <Link href = "https://github.com/miniloda/Aplaya" noopener = "true" target = "_blank">
+                        <img src={Git_icon} alt="Github" height = "20"></img>
+                    </Link>
+                    </span>
                 </Box>
             </Grid>
         </Grid>
