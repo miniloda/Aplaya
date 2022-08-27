8b834541cd8d8c2af25b0029cf78f48e330dd5cd
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-08-27 23:17:18 +0800

    added contact us form

--- src/Components/MainLayout/Contact.js
+++ src/Components/MainLayout/Contact.js
@@ -10,27 +10,73 @@ import Card from '@mui/material/Card';
 import Paper from '@mui/material/Paper';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { useEffect } from 'react';
 
 export default function Contact(){
+  useEffect(() => {
+    const handleResize = () => {
+      const formHeight = document.querySelector("#field-grid").offsetHeight;
+      document.querySelector(".contact-info").style.height = formHeight + "px";
+    
+    }
+    handleResize();
+    return () => {
+        window.removeEventListener("resize", handleResize);
+    }
+});
+
     return(
-    <>
-        <Container>
-    <Box sx ={{height: 100}}>
-        <div height = "100px"></div>
-    </Box>
-</Container>
-    <Grid container spacing = {3} mt = {5} id = "contact">
-        <Grid item xs = {0} sm = {4}></Grid>
-        <Grid item xs = {12} sm = {4}>
+        <>
+    <Container id = "contact">
+        <Box sx = {{display: "flex", flexDirection: "column", alignItems: "center", width: "100%"}} my = {5} >
+            <h1>Interested? Contact Us!</h1>
+        </Box>
+    </Container>
+    <Grid container spacing={0} gutterBottom>
+        <Grid item xs = {0} sm = {1} md = {2} xl = {3}></Grid>
+        <Grid item xs={10} sm = {10} md = {4} xl = {2}>
+        <div id="contact-container">
+            <Paper elevation = {24}>
+            <div className="contact-info">
+                <h4>
+                    Contact Information
+                </h4>
+                <p>Contact us in our social media accounts, email, phone number, or fill in the form and we will get back to you in 24 hours</p>
+                <div className="icon-text">
+                <i class="fa-solid fa-phone"></i>
+                    <span>+63919 999 6897</span>
+                </div>
+                <div className="icon-text">
+                <i class="fa-solid fa-envelope"></i>
+                <span>aplayafilms@gmail.com</span>
+                </div>
+                <div className="icon-text">
+                <i class="fa-solid fa-location-dot"></i>
+                    <span>Tagbilaran City, Bohol</span>
+                    </div>
+                <div className="social-media">
+                    <a href= "#" className="icon-circle">
+                    <i class="fa-brands fa-facebook fa"></i>
+                    </a>
+                    <a href= "#" className="icon-circle">
+                    <i class="fa-brands fa-instagram fa"></i>
+                    </a>
+                    <a href= "#" className="icon-circle">
+                    <i class="fa-brands fa-twitter fa"></i>
+                    </a>
+                </div>
+            </div>
+        </Paper>
+        </div>
+        </Grid>
+        <Grid item xs = {12} md = {4} id = "field-grid">
           <Paper elevation = {24} square >
         <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto", bgcolor : "primary" }}>
           <CardContent>
-            <Typography gutterBottom variant="h5">
-              Contact Us
-          </Typography> 
-            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
-              Fill up the form and we will try our best to get back to you within 24 hours.
-          </Typography>
             <form>
               <Grid container spacing={1} >
                 <Grid xs={12} sm={6} item>
@@ -45,6 +91,18 @@ export default function Contact(){
                 <Grid item xs={12}>
                   <TextField type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required />
                 </Grid>
+                <Grid item xs = {12}>
+                <FormLabel id="demo-row-radio-buttons-group-label">Message Category</FormLabel>
+                        <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Query" />
+                        <FormControlLabel value="male" control={<Radio />} label="Reservation" />
+                        <FormControlLabel value="other" control={<Radio />} label="Concern" />
+                    </RadioGroup>
+                </Grid>
                 <Grid item xs={12}>
                   <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
                 </Grid>
@@ -58,9 +116,7 @@ export default function Contact(){
         </Card>
             </Paper>
         </Grid>
-
-      </Grid>
-      </>
+        </Grid>
+        </>
     )
-
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./assets/Memphis-LT-Std-Light.otf";
-
+import './styles/main.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
